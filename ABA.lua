c16864456d7aa35a1ea91678ab2704cca4739522
Ace <scoopmayy@gmail.com>
2026-01-23 18:52:07 +0100

    Add files via upload

---
+++ ABA.lua
@@ -0,0 +1,15 @@
+-- BestAreaTpPremium_obfuscated.lua
+-- Auto TP to best training area (Strength / Durability / Chakra)
+-- Logic will be added later by you
+
+_G.ToggleAutoTpBestStrength = function(enabled)
+    -- Your code here (start / stop for Strength)
+end
+
+_G.ToggleAutoTpBestDurability = function(enabled)
+    -- Your code here (start / stop for Durability)
+end
+
+_G.ToggleAutoTpBestChakra = function(enabled)
+    -- Your code here (start / stop for Chakra)
+end
